96201bb518927b311a4fce2be602257e87006e39
frdvorak <frantisek.dvorak011@gmail.com>
2018-08-24 22:42:24 +0100

    added edit and create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './components/list/list.component';
+import { EditComponent } from './components/edit/edit.component';
+import { CreateComponent } from './components/create/create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    EditComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
